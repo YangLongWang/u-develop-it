f957e29c5d8a3f6972fca81281292f94bd47bbcd
yang <joey674901205@gmail.com>
2022-07-17 14:25:37 -0400

    use mysql2 to connnect database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        // your MySQL password
+        password: 'yangbao94Wly',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
